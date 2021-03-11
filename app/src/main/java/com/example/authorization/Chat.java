f607f2287f2ea2d4b6889e6df1ca371bd72a3e5b
cvidgi <andrrey_tver@mail.ru>
2021-03-11 19:52:34 +0300

    немного чата

--- app/src/main/java/com/example/authorization/Chat.java
+++ app/src/main/java/com/example/authorization/Chat.java
@@ -1,14 +1,32 @@
 package com.example.authorization;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
 
 public class Chat extends AppCompatActivity {
+    private ImageView app;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
+        app = findViewById(R.id.app);
+
+        app.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), DetailedChat.class);
+                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                startActivity(intent);
+                overridePendingTransition(0,0);
+
+            }
+        });
     }
 }
