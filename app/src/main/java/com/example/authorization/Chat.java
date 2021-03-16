76605e11e58af3524f03dedba9c271a9ee3c2a45
Cvidgi <andrrey_tver@mail.ru>
2021-03-16 16:10:47 +0300

    Небольшие изменения в авторизации

--- app/src/main/java/com/example/authorization/Chat.java
+++ app/src/main/java/com/example/authorization/Chat.java
@@ -3,12 +3,29 @@ package com.example.authorization;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.database.FirebaseListAdapter;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class Chat extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+
     }
+
+
 }
