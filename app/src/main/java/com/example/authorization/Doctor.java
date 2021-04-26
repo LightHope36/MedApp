5ec94ee71a5ab33024c202b931bf5cc4759c7057
Cvidgi <andrrey_tver@mail.ru>
2021-04-26 18:14:09 +0300

    sdf

--- app/src/main/java/com/example/authorization/Doctor.java
+++ app/src/main/java/com/example/authorization/Doctor.java
@@ -1,19 +1,13 @@
 package com.example.authorization;
 
 public class Doctor {
-    private String Name;
-    private String proffession;
+    private String Text;
 
-    public Doctor(String Name, String proffession){
-        this.Name = Name;
-        this.proffession = proffession;
+    public String getText() {
+        return Text;
     }
 
-    public String getProffession(){
-        return proffession;
-    }
-
-    public String getName() {
-        return Name;
+    public void setText(String text) {
+        Text = text;
     }
 }
