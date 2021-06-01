80fd4329c80a62188e923d18fd66b8b09d8178f4
Slava <s.krivosheykin@gmail.com>
2021-06-01 19:54:28 +0300

    продолжаю работу над анимациями

--- app/src/main/java/com/example/authorization/MainPage2.java
+++ app/src/main/java/com/example/authorization/MainPage2.java
@@ -6,6 +6,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
 
 public class MainPage2 extends AppCompatActivity {
     private ConstraintLayout cs;
@@ -25,12 +27,19 @@ public class MainPage2 extends AppCompatActivity {
         dialogs = findViewById(R.id.cs_to_dial);
         profile = findViewById(R.id.profile_in_main_page);
 
+        ConstraintLayout constraintLayout1 = findViewById(R.id.constraintLayout_to_animation_in_main_page);
+        ConstraintLayout constraintLayout2 = findViewById(R.id.constraintLayout_to_animation_in_dialogs);
+
+        Animation an1 = AnimationUtils.loadAnimation(this, R.anim.main_left);
+        Animation an2 = AnimationUtils.loadAnimation(this, R.anim.second_left);
+
         dialogs.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(getApplicationContext(), Dialogs.class);
                 intent.putExtra("number", number);
-                intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION);
+                constraintLayout1.startAnimation(an1);
+                constraintLayout2.startAnimation(an2);
                 startActivity(intent);
                 overridePendingTransition(0, 0);
             }
